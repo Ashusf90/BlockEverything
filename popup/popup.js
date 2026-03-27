e68ca4ec9e1939d1784afcd23a1a588c496fec6c
Harshit Gupta <153393197+Ashusf90@users.noreply.github.com>
2026-03-27 12:13:15 +0530

    Update popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,23 +1,37 @@
-
 document.addEventListener("DOMContentLoaded", () => {
     const btn = document.getElementById("toggle-btn");
     const btnImage = document.getElementById("toggle-btn-image");
+
     const btnImageSources = {
         off: "../assets/visuals/icons/main-button/off/vector-theme-1.svg",
-        on : "../assets/visuals/icons/main-button/on/vector-theme-1.svg"
-    }
+        on: "../assets/visuals/icons/main-button/on/vector-theme-1.svg"
+    };
+
     let isBlocking = false;
 
-    btn.addEventListener("click", () => {
-    isBlocking = !isBlocking;
+    btn.addEventListener("click", async () => {
+        isBlocking = !isBlocking;
 
-    console.log(btnImageSources.off);
-    if (isBlocking) {
-        btnImage.src = btnImageSources.off;
-        btnImage.alt = "off";
-    } else {
-        btnImage.src = btnImageSources.on;
-        btnImage.alt = "on";
-    }
+        // 🔁 Toggle UI
+        if (isBlocking) {
+            btnImage.src = btnImageSources.on;
+            btnImage.alt = "on";
+
+            //  GET CURRENT TAB URL
+            let [tab] = await chrome.tabs.query({
+                active: true,
+                currentWindow: true
+            });
+
+            const url = tab.url;
+            const domain = new URL(url).hostname;
+
+            console.log("URL:", url);
+            console.log("Website:", domain);
+
+        } else {
+            btnImage.src = btnImageSources.off;
+            btnImage.alt = "off";
+        }
     });
 });
